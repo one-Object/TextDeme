6e490d3e9bbae079b8853726895b658bc0610151
one-Object <635077862@qq.com>
2018-11-18 11:11:47 +0800

    添加组件

---
+++ src/component/home/index.jsx
@@ -0,0 +1,13 @@
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>首页666</div>
+         );
+    }
+}
+ 
+export default Home;
