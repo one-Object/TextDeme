3988c0db30348ed4b1b1823f3d81a43061732809
one-Object <635077862@qq.com>
2020-01-11 18:20:55 +0800

    添加第四个tab

--- src/component/home/index.jsx
+++ src/component/home/index.jsx
@@ -39,6 +39,7 @@ class Home extends Component {
                     <TabPane tab="可编辑表格" key="1"><EditTable/></TabPane>
                     <TabPane tab="handsontable" key="2"><HotTableDemo /></TabPane>
                     <TabPane tab="哈哈" key="3"><SearchResult/></TabPane>
+                    <TabPane tab="第四个tab" key="4"><SearchResult/></TabPane>
                 </Tabs>
                 <PersonModal/>
             </div>
